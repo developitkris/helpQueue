7ce5a7d0bfd9825cebf6f92bf11bf8248ef2440c
Kristi <krsy3ii@yahoo.com>
2018-06-26 11:09:21 -0700

    add .jsx file to project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Help Queue</title>
-    <script src="https://unpkg.com/react@15/dist/react.js"></script>
-    <script src="https://unpkg.com/react-dom@15/dist/react-dom.js"></script>
-  </head>
-  <body>
-    <div id="react-app-root">
-    </div>
-  </body>
-  <script src="src/index.js"></script>
-</html>
+let heading = React.createElement('h1', {}, 'Help Queue');
+let ticketLocation = React.createElement('h3', {}, '3a');
+let ticketNames = React.createElement('h3', {}, 'Kris and Marcus');
+let ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
+let app = React.createElement('div', {}, 'heading');
+
+ReactDOM.render(
+  app,
+  document.getElementById('react-app-root')
+);
