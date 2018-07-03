e3972e7c4236c489b72d5938a519ce2110f5821f
Kristi <krsy3ii@yahoo.com>
2018-07-03 10:42:23 -0700

    incorporate lifecycle methods to update site per minute

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Header from "./Header";
 import TicketList from "./TicketList";
 import { Switch, Route } from 'react-router-dom';
-// import NewTicketForm from './NewTicketForm';
 import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
+import Moment from 'moment';
 
 class App extends React.Component{
   constructor(props){
@@ -14,14 +14,59 @@ class App extends React.Component{
       };
       this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
   }
+
+  componentDidMount(){
+    console.log('componentDidMount');
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+      60000
+    );
+  }
+
+  componentWillUnmount(){
+    console.log('componentWillUnmount');
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  componentWillMount() {
+   console.log('componentWillMount');
+  }
+
+   componentWillReceiveProps() {
+     console.log('componentWillReceiveProps');
+   }
+
+   shouldComponentUpdate() {
+     console.log('shouldComponentUpdate');
+     return true;
+   }
+
+   componentWillUpdate() {
+     console.log('componentWillUpdate');
+   }
+
+   componentDidUpdate() {
+     console.log('componentDidUpdate');
+   }
+
+  updateTicketElapsedWaitTime(){
+    console.log('check');
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );
+    this.setState({masterTicketList: newMasterTicketList})
+  }
+
   handleAddingNewTicketToList(newTicket){
     var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true)
     newMasterTicketList.push(newTicket);
     this.setState({masterTicketList: newMasterTicketList});
   }
 
   render(){
-  return (
+    return (
     <div>
       <Header/>
       <Switch>
