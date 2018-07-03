757d05f72d1fa0ccae79d60071eb39e4d30662aa
Kristi <krsy3ii@yahoo.com>
2018-07-03 16:45:37 -0700

    update to include new slice of state for admin, display ticket details for click event

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -4,21 +4,27 @@ import TicketList from './TicketList';
 import TicketDetail from './TicketDetail';
 
 function Admin(props){
-  console.log(props.currentRouterPath);
+  let optionalSelectedTicketContent = null;
+  if (props.selectedTicket != null){
+    optionalSelectedTicketContent =  <TicketDetail selectedTicket={props.selectedTicket}/>;
+  }
   return (
     <div>
       <h2>Admin</h2>
-      <TicketDetail />
+      {optionalSelectedTicketContent}
       <TicketList
         ticketList={props.ticketList}
-        currentRouterPath={props.currentRouterPath} />
+        currentRouterPath={props.currentRouterPath}
+        onTicketSelection={props.onTicketSelection}/>
     </div>
   );
 }
 
 Admin.proptypes = {
   ticketList: PropTypes.array,
-  currentRouterPath: PropTypes.string
+  currentRouterPath: PropTypes.string.isRequired,
+  onTicketSelection: PropTypes.func.isRequired,
+  selectedTicket: PropTypes.object
 };
 
 export default Admin;
