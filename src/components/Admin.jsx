e4cda7b69476e2f53c721a87d206a7c6336317cb
Kristi <krsy3ii@yahoo.com>
2018-07-03 16:10:36 -0700

    add another state to App.jsx for Admin view

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TicketList from './TicketList';
+import TicketDetail from './TicketDetail';
 
 function Admin(props){
   console.log(props.currentRouterPath);
   return (
     <div>
       <h2>Admin</h2>
+      <TicketDetail />
       <TicketList
         ticketList={props.ticketList}
         currentRouterPath={props.currentRouterPath} />
